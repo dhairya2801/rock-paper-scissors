246fdbb405a235c97a9989e1dd9ddff7e77cc598
Dhairya <dhairyashah.ai@gmail.com>
2025-09-20 09:33:02 -0400

    Added basic DOM methods
    This is a single round play version where
    the round starts when the user presses one
    of the three buttons.

--- script.js
+++ script.js
@@ -1,75 +1,83 @@
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+let hScore = 0;
+let cScore = 0;
 
-    for (i=0; i<5; ++i) {
-        let round = playRound();
-        if (round === 'H') {
-            ++humanScore;
+const buttons = document.querySelectorAll('button');
+let hChoice;
+buttons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        switch (btn.className) {
+            case 'rockBtn':
+                hChoice = 'rock';
+                break;
+            case 'paperBtn':
+                hChoice = 'paper';
+                break;
+            case 'scissorBtn':
+                hChoice = 'scissor';
+                break;
         }
-        else if (round === 'C' ) {
-            ++computerScore;
-        }
+        const humanSelection = document.querySelector('.humanSelection');
+        humanSelection.textContent = `You selected: ${hChoice}`;
+
+        playRound(hChoice);
+    })
+})
+
+function getComputerChoice() {
+    let choice = Math.floor(10*Math.random());
+
+    let out = "";
+    if (choice <= 3) {
+        out = "rock";
     }
-
-    function getComputerChoice() {
-        let choice = Math.floor(10*Math.random());
-
-        let out = "";
-        if (choice <= 3) {
-            out = "rock";
-        }
-        else if (choice >= 7) {
-            out = "scissors";
-        }
-        else {
-            out = "paper";
-        }
-        return out;
+    else if (choice >= 7) {
+        out = "scissors";
     }
-
-    function getHumanChoice() {
-        return prompt("Select your choice (rock/paper/scissor): ")
+    else {
+        out = "paper";
     }
-
-    function playRound() {
-        let human = getHumanChoice();
-        let computer = getComputerChoice();
-        if (human === computer) {
-            return console.log("Tied")
-        }
-        if (human === "rock") {
-            if (computer === "paper") {
-                console.log("Computer selected paper. Computer wins!");
-                return 'C';
-            }
-            else {
-                console.log("Computer selected scissors. You win!");
-                return 'H'
-            }
-        }
-        else if (human === "paper") {
-            if (computer === "rock") {
-                console.log("Computer selected rock. You win!");
-                return 'H';
-            }
-            else {
-                console.log("Computer selected scissors. Computer wins!");
-                return 'C';
-            }
-        }
-        else {
-            if (computer === "rock") {
-                console.log("Computer selected rock. Computer wins!");;
-                return 'C';
-            }
-            else {
-                console.log("Computer selected paper. You win!");
-                return 'H'
-            }
-        }
-    }
-    return console.log(`You: ${humanScore}, Computer: ${computerScore}`);
+    return out;
 }
 
-playGame();
+function playRound(human) {
+    let computer = getComputerChoice();
+    const computerSelection = document.querySelector('.computerSelection');
+    computerSelection.textContent = `Computer selected: ${computer}`;
+
+    if (human === computer) {
+        return console.log('tie');
+    }
+    if (human === "rock") {
+        if (computer === "paper") {
+            console.log("Computer selected paper. Computer wins!");
+            ++cScore;
+        }
+        else {
+            console.log("Computer selected scissors. You win!");
+            ++hScore;
+        }
+    }
+    else if (human === "paper") {
+        if (computer === "rock") {
+            console.log("Computer selected rock. You win!");
+            ++hScore;
+        }
+        else {
+            console.log("Computer selected scissors. Computer wins!");
+            ++cScore;
+        }
+    }
+    else {
+        if (computer === "rock") {
+            console.log("Computer selected rock. Computer wins!");;
+            ++cScore;
+        }
+        else {
+            console.log("Computer selected paper. You win!");
+            ++hScore;
+        }
+    }
+
+    const score = document.querySelector('.score');
+    score.textContent = `Score: You: ${hScore}, Computer: ${cScore}`;
+}
